20c44b13722d3b99cff9345328de0dc6d0f2db28
yongjian-h <33388713+yongjian-h@users.noreply.github.com>
2018-01-10 19:41:53 +0800

    Revert "老王"
    This reverts commit c5046019551f6633541c0a3e17186ebcf3325d58.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,32 @@
+<<<<<<< HEAD
 module.exports = {
 
 };
+=======
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "commonjs": true,
+//         "es6": true
+//     },
+//     "extends": "eslint:recommended",
+//     "rules": {
+//         "indent": [
+//             "error",
+//             4
+//         ],
+//         "linebreak-style": [
+//             "error",
+//             "unix"
+//         ],
+//         "quotes": [
+//             "error",
+//             "single"
+//         ],
+//         "semi": [
+//             "error",
+//             "always"
+//         ]
+//     }
+// };
+>>>>>>> parent of c504601... 老王
