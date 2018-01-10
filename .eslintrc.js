82cacd6c25b201078e6da37237d1c53845618b34
yongjian-h <33388713+yongjian-h@users.noreply.github.com>
2018-01-10 20:49:39 +0800

    123

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,27 @@
-
 module.exports = {
-asd
+  "extends": "eslint:recommended",
+
+  //规则
+  "rules":{
+    "no-console":["error",{//012 2代表error
+      "allow":["warn","error","info"]
+    }]
+  },
+  //解析器
+  "parser":"babel-eslint",
+  //解析器参数
+  "parserOptions":{
+    "ecmaVersion":6,
+    "sourceType":"script"
+  },
+  //环境
+  "env":{
+    "node":true,
+    "es6":true,
+    "mocha":true
+  },
+  "globals":{
+    //"window":true
+  }
 };
 
-// module.exports = {
-//     "env": {
-//         "browser": true,
-//         "commonjs": true,
-//         "es6": true
-//     },
-//     "extends": "eslint:recommended",
-//     "rules": {
-//         "indent": [
-//             "error",
-//             4
-//         ],
-//         "linebreak-style": [
-//             "error",
-//             "unix"
-//         ],
-//         "quotes": [
-//             "error",
-//             "single"
-//         ],
-//         "semi": [
-//             "error",
-//             "always"
-//         ]
-//     }
-// };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 
+
+// eslint-disable-next-line
 console.log(123);
+
 // alert(11);
+console.log(123);
