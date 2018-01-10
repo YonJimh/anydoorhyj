c5046019551f6633541c0a3e17186ebcf3325d58
yongjian-h <33388713+yongjian-h@users.noreply.github.com>
2018-01-10 19:36:51 +0800

    老王

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,3 @@
-// module.exports = {
-//     "env": {
-//         "browser": true,
-//         "commonjs": true,
-//         "es6": true
-//     },
-//     "extends": "eslint:recommended",
-//     "rules": {
-//         "indent": [
-//             "error",
-//             4
-//         ],
-//         "linebreak-style": [
-//             "error",
-//             "unix"
-//         ],
-//         "quotes": [
-//             "error",
-//             "single"
-//         ],
-//         "semi": [
-//             "error",
-//             "always"
-//         ]
-//     }
-// };
+module.exports = {
+asd
+};
