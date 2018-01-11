0ad7de38d3f79aa2c07167f62d20561c4a8d8f2b
yongjian-h <33388713+yongjian-h@users.noreply.github.com>
2018-01-11 19:24:08 +0800

    Revert "Revert "asd""
    This reverts commit c1596a5a49043240d80aaff52791b2308af3f1a5.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+const http=require('http');
+const chalk=require('chalk');
+const conf=require('./config/defaultConfig');
 
-<<<<<<< HEAD
 const server = http.createServer((req , res)=>{
     // res.statusCode=233;
     // res.setHeader('Content-Type','text/html; charset=utf-8');
@@ -26,8 +28,6 @@ server.listen(conf.port,conf.hostname,()=>{
       const addr = `http://${conf.hostname}:${conf.port}`;
       console.info(`Server started at ${chalk.green(addr)}`);
 });
-=======
->>>>>>> parent of feb077d... asd
 
 // eslint-disable-next-line
 console.log(123);
