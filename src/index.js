d7727a72295f8ec01e7df676e9355cec75650b4d
yongjian-h <33388713+yongjian-h@users.noreply.github.com>
2018-01-11 19:13:48 +0800

    Revert "Revert "server""
    This reverts commit ba08efc5fbb568565b3c23afcc7f82738fa1ff07.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
+const http=require('http');
+const chalk=require('chalk');
+const conf=require('./config/defaultConfig');
 
+const server = http.createServer((req , res)=>{
+    // res.statusCode=233;
+    // res.setHeader('Content-Type','text/html; charset=utf-8');
+    // res.setHeader('Content-Type','text/plain');
+    res.writeHead(233, {'Content-Type': 'text/html; charset=utf-8'});
+    res.write(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+    </head>
+    <body>
+        123456453
+    </body>
+    </html>
+    `);
+    res.end();
+});
 
-// eslint-disable-next-line
-console.log(123);
-
-// alert(11);
-
-// console.log(123);
-// console.log(123);
-
-
+server.listen(conf.port,conf.hostname,()=>{
+      const addr = `http://${conf.hostname}:${conf.port}`;
+      console.info(`Server started at ${chalk.green(addr)}`);
+});
