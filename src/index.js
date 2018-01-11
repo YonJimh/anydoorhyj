a09f0c488ab108a5ac2752cb0a3eca560df2dbaf
yongjian-h <33388713+yongjian-h@users.noreply.github.com>
2018-01-11 18:33:00 +0800

    Revert "server"
    This reverts commit d2c1bf2e1e59ab6f0f8a53e8f662ede8bffb5791.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 
-
 const server = http.createServer((req , res)=>{
     // res.statusCode=233;
     // res.setHeader('Content-Type','text/html; charset=utf-8');
@@ -20,14 +19,6 @@ const server = http.createServer((req , res)=>{
     </html>
     `);
     res.end();
-});
-// sad
-server.listen(conf.port,conf.hostname,()=>{
-      const addr = `http://${conf.hostname}:${conf.port}`;
-      console.info(`Server started at ${chalk.green(addr)}`);
-});
-=======
-
 
 // eslint-disable-next-line
 console.log(123);
@@ -38,4 +29,3 @@ console.log(123);
 // console.log(123);
 
 
->>>>>>> parent of d2c1bf2... server
