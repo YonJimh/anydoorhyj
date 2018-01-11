73894ccca2d13e4ef12146fc179b3ca523c9f6f1
yongjian-h <33388713+yongjian-h@users.noreply.github.com>
2018-01-11 18:31:29 +0800

    asd
    asd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-<<<<<<< HEAD
+
 const server = http.createServer((req , res)=>{
     // res.statusCode=233;
     // res.setHeader('Content-Type','text/html; charset=utf-8');
@@ -26,19 +26,3 @@ server.listen(conf.port,conf.hostname,()=>{
       const addr = `http://${conf.hostname}:${conf.port}`;
       console.info(`Server started at ${chalk.green(addr)}`);
 });
-=======
-=======
->>>>>>> parent of d2c1bf2... server
-
-// eslint-disable-next-line
-console.log(123);
-
-// alert(11);
-
-// console.log(123);
-// console.log(123);
-
-
->>>>>>> parent of d2c1bf2... server
-=======
->>>>>>> parent of d2c1bf2... server
